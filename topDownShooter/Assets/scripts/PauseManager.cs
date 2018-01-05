600f3dc21e5e5cee50c166252b37aadbe1d74687
CaptainHaddok <warre.bruyninckx@hotmail.com>
2018-01-05 20:05:56 +0100

    pause menu
    Made a pause menu

---
+++ topDownShooter/Assets/scripts/PauseManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+    public GameObject menu;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Menuswitch();
+        }
+    }
+
+    public void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Menuswitch()
+    {
+        menu.active = !menu.active;
+        if (menu.active == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}

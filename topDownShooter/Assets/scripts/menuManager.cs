f180780ebe7e9a8e4ce81ff45507861398f89899
CaptainHaddok <warre.bruyninckx@hotmail.com>
2018-01-05 19:02:44 +0100

    import
    add my existing project

---
+++ topDownShooter/Assets/scripts/menuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour {
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ topDownShooter/Assets/scripts/playerControler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class playerControler : MonoBehaviour {
+    //movement
+    public int speed = 10;
+    
+    //jumps
+    public int maxjumps = 1;
+    public int currentJumps = 1;
+    public int jumpHeight = 10  ;
+
+    public Rigidbody rb;
+    public Transform tf;
+    private Vector3 position;
+
+    void Start ()
+    {
+        rb.GetComponent<Rigidbody>();
+        tf.GetComponent<Transform>();
+	}
+
+    void Update()
+    {
+        if (tf.position.y< -5)
+        {
+            die();
+        }
+        movement();
+        jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (maxjumps != currentJumps)
+        {
+            currentJumps = maxjumps;
+        }
+    }
+
+    private void jump()
+    {
+        float Jump = Input.GetAxisRaw("Jump");
+        Vector3 JumpVec = new Vector3(0.0f, Jump, 0.0f);
+        if (currentJumps > 0 && Jump != 0)
+        {
+            rb.AddForce(JumpVec * jumpHeight);
+            currentJumps--;
+        }
+    }
+
+    private void movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    private void die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
